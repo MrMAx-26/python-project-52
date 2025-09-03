9d43318353fee56b09bce78580cc303b3c8f4244
Максим Шевцов <shevtsov_stv@mail.ru>
2025-09-03 19:58:13 +0300

    Fix templates

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.IndexView.as_view(), name="index"),
     path("users/", include("task_manager.users.urls")),
-    path("login/", views.LoginView.as_view(), name="login"),
+    path("login/", views.LogoutView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("statuses/", include("task_manager.statuses.urls")),
     path("tasks/", include("task_manager.tasks.urls")),
