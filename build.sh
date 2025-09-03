738e17f2a54492ce70cdfd5aa25715a062b2f1f2
Максим Шевцов <shevtsov_stv@mail.ru>
2025-09-04 02:08:52 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,31 +1,8 @@
 #!/usr/bin/env bash
-
-# Убедитесь, что скрипт прерывается при ошибках
-set -e
-
-# Скачиваем и устанавливаем uv
-echo "Скачиваем и устанавливаем uv..."
+# скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Создание виртуального окружения, если оно не существует
-if [ ! -d ".venv" ]; then
-    echo "Создание виртуального окружения..."
-    python -m venv .venv
-fi
-
-# Активируем виртуальное окружение
-echo "Активируем виртуальное окружение..."
-source .venv/bin/activate
-
-# Установка зависимостей
-echo "Установка зависимостей..."
-pip install -r requirements.txt
-
-# Выполнение команд Makefile
-echo "Выполняем команды Makefile..."
-make install
-make collectstatic
-make migrate
-
-echo "Сборка и настройка завершены."
+# здесь добавьте все необходимые команды для установки вашего проекта
+# команду установки зависимостей, сборки статики, применения миграций и другие
+make install && make collectstatic && make migrate
