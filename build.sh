b8f1613733d01f841574b2c95091aab6427cb750
Максим Шевцов <shevtsov_stv@mail.ru>
2025-09-04 03:42:38 +0300

    fix

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ source $HOME/.local/bin/venv
 
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
+# Укажите явный путь к интерпретатору Python в виртуальном окружении
 VENV_PYTHON="$PWD/venv/bin/python"
 
 # Проверьте, что Python использует правильное окружение
@@ -15,3 +16,6 @@ $VENV_PYTHON -m pip install -r requirements.txt
 # Запустите миграции и статику, используя Python из виртуального окружения
 $VENV_PYTHON manage.py collectstatic --noinput
 $VENV_PYTHON manage.py migrate
+
+# Запустите сервер
+$VENV_PYTHON manage.py runserver
